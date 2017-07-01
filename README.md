6df073c42d3cb5b5300cfb081ed62be220d5a062
Conner Gillette <cg1100@uw.edu>
2017-07-01 12:27:17 -0700

    Setup intial project

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# mystance
-Simple tool to share your stance and see that of others.
+# MyStance
+Simple tool to share your stance and see those of others.
---
+++ back-end/server.js
@@ -0,0 +1,6 @@
+var express = require("express");
+var server = express();
+
+server.listen(3000, function() {
+	console.log("Server listening on port 3000");
+});
