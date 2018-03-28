3a62a1c30597020cdde4cc503ec4fc67513b9e45
Conner Gillette <cg1100@uw.edu>
2018-03-28 14:35:51 -0700

    Add Demo Video button

--- back-end/server.js
+++ back-end/server.js
@@ -16,25 +16,20 @@ var cors = require('./services/cors');
 server.use(cors);
 server.use(bodyParser.json());
 
-server.use(express.static('front-end/dist/'))
+// server.use(express.static('front-end/dist/'));
 
-// server.use(function(req, res) {
-// 	// Use res.sendfile, as it streams instead of reading the file into memory.
-// 	res.sendfile('/front-end/dist/index.html');
-// });
-
-server.get('/', function(req, res) {
-	res.sendfile('front-end/dist/index.html')
+server.get('/', function (req, res) {
+	res.sendfile('front-end/dist/index.html');
 });
 
-server.get('/topic/:id', function(req, res, next) {
+server.get('/topic/:id', function (req, res, next) {
 	res.id = req.params.id;
 	next();
 }, topic.get);
 
 server.post('/topic/add', topic.post);
 
-server.post('/topic/:id/:side/reason/add', function(req, res, next) {
+server.post('/topic/:id/:side/reason/add', function (req, res, next) {
 	res.id = req.params.id;
 	res.side = req.params.side;
 	next();
@@ -48,8 +43,8 @@ server.post('/user/login', auth.login);
 // TODO: Protect mLab login credentials
 
 // Mongo connection
-mongoose.connect("mongodb://heroku_1d5zllb6:tu1volnhoufb2smcl4tuc8uui7@ds115712.mlab.com:15712/heroku_1d5zllb6", function(err, db) {
-	// mongoose.connect("mongodb://localhost:27017/test", function(err, db) {
+// mongoose.connect("mongodb://heroku_1d5zllb6:tu1volnhoufb2smcl4tuc8uui7@ds115712.mlab.com:15712/heroku_1d5zllb6", function(err, db) {
+mongoose.connect("mongodb://localhost:27017/test", function (err, db) {
 
 	if (!err) {
 		console.log("Connected to Mongo");
@@ -59,12 +54,12 @@ mongoose.connect("mongodb://heroku_1d5zllb6:tu1volnhoufb2smcl4tuc8uui7@ds115712.
 })
 
 // Server listener
-server.listen(process.env.PORT || 4000, function() {
-	server.use(express.static(__dirname));
-	// server.use(gzippo.staticGzip("" + __dirname + "/dist"));
-	console.log("Server listening on port " + process.env.PORT);
-});
-
-// server.listen(4000, function() {
-// 	console.log("Server listening on port 4000");
+// server.listen(process.env.PORT || 4000, function() {
+// 	server.use(express.static(__dirname));
+// 	// server.use(gzippo.staticGzip("" + __dirname + "/dist"));
+// 	console.log("Server listening on port " + process.env.PORT);
 // });
+
+server.listen(4000, function () {
+	console.log("Server listening on port 4000");
+});
